9e354fc2e3a669fdc5813d261ad531a4ea5f473e
Aman Choudhary <hoonwangdu@gmail.com>
2024-10-24 02:25:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,41 +12,44 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "mykey" {
-  key_name = "mykey"
+  key_name   = "mykey"
   public_key = file("/home/ubuntu/.ssh/mykey.pub")
 }
 
-resource "aws_default_vpc" "vpc" {
-}
+resource "aws_default_vpc" "vpc" {}
 
 resource "aws_security_group" "allow_traffic" {
-  name = "allow_traffic"
+  name        = "allow_traffic"
   description = "allow ssh inbound traffic - ingress rules"
-  vpc_id = aws_default_vpc.vpc.id
-  ingress {
-     description = " "
-     from_port = 22
-     to_port = 22
-     protocol = "tcp"
-     cidr_blocks = ["0.0.0.0/0"]
-     }
+  vpc_id      = aws_default_vpc.vpc.id
 
-   tags = {
-      name = "allow_traffic"
-       }
+  ingress {
+    description = "SSH ingress"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_traffic"
+  }
 }
 
 resource "aws_instance" "my_ec2" {
-     count = 2
-     ami = "ami-0866a3c8686eaeeba"
-     instance_type = "t2.micro"
+  count         = 2
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
 
-     key_name = aws_key_pair.mykey.key_name
+  key_name         = aws_key_pair.mykey.key_name
+  security_groups  = [aws_security_group.allow_traffic.name]
 
-     security_groups = [aws_security_group.allow_traffic.name]
+  tags = {
+    Name = "SecuredInstance-${count.index + 1}"
+  }
+}
 
-     tags = {
-        Name = "SecuredInstance-${count.index +1}"
-            }
-
+output "instance_public_ips" {
+  description = "Public IPs of the EC2 instances"
+  value       = aws_instance.my_ec2[*].public_ip
 }
